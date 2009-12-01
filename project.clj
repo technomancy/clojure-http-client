6254aa2f28cc9567ab54e898a653c3c23e786544
Phil Hagelberg <technomancy@gmail.com>
2009-11-30 20:56:11 -0800

    Don't use org.clojure group.

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject org.clojure/clojure-http-client "1.0.0-SNAPSHOT"
+(defproject clojure-http-client "1.0.0-SNAPSHOT"
   :description "An HTTP client for Clojure."
   :dependencies [[org.clojure/clojure-contrib "1.0-SNAPSHOT"]])
