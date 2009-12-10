985b64316b2a2ecd9874185cafef466b8719d2a9
Phil Hagelberg <technomancy@gmail.com>
2009-12-10 12:14:38 -0800

    Depend on master version of clojure.

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject clojure-http-client "1.0.0-SNAPSHOT"
   :description "An HTTP client for Clojure."
-  :dependencies [[org.clojure/clojure-contrib "1.0-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]])
