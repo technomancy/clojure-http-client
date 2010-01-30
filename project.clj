67b4e08aab30160dc32aa3cda1e00a8c0143ff4e
Phil Hagelberg <technomancy@gmail.com>
2010-01-30 15:27:40 -0800

    Update Clojure/contrib/swank dependency versions.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject clojure-http-client "1.0.0-SNAPSHOT"
   :description "An HTTP client for Clojure."
-  :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
+  :dev-dependencies [[swank-clojure "1.1.0"]])
