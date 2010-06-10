cc61cb1da403c9211728335f318f9d78a89612a3
Phil Hagelberg <technomancy@gmail.com>
2010-06-10 14:25:30 -0700

    Bump version to 1.1.0-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-http-client "1.0.1"
+(defproject clojure-http-client "1.1.0-SNAPSHOT"
   :description "An HTTP client for Clojure."
   :dev-dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                      [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
