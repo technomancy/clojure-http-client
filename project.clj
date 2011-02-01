e0d1103790142d11d7315c824c415d9215e9071d
Phil Hagelberg <technomancy@gmail.com>
2011-02-01 09:44:48 -0800

    Release 1.1.0; this project is done. Use clj-http.

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject clojure-http-client "1.1.0-SNAPSHOT"
+(defproject clojure-http-client "1.1.0"
   :description "An HTTP client for Clojure."
   :dev-dependencies [[org.clojure/clojure "1.2.0"]
                      [org.clojure/clojure-contrib "1.2.0"]
                      [swank-clojure "1.2.1"]
-                     [autodoc "0.7.0"]])
+                     [autodoc "0.7.1" :exclusions [org.clojure/clojure-contrib
+                                                   org.clojure/clojure
+                                                   ant/ant ant/ant-launcher]]])
